17af5716d772fd92e795a4fb49f89cbec5c2a15a
cooperbraun13 <cooperjbraun13@gmail.com>
2025-06-03 06:20:37 -0700

    refactored code and moved the num of words function to a stats.py file, also added the python executable to gitignore file

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
+from stats import num_of_words
+
 def get_book_text(filepath):
   with open(filepath) as f:
     return f.read()
-  
-def num_of_words(text):
-  words = text.split()
-  return len(words)
   
 def main():
   entire_book = get_book_text("books/frankenstein.txt")
